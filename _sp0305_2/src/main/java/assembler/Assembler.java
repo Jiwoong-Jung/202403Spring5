a3648a8fd9af115e67bb51a8860f900f2c5cf57c
Jiwoong-Jung <jjw981119@naver.com>
2024-03-05 15:48:55 +0900

    스프링책 3장 예제 프로젝트

--- _sp0305_2/src/main/java/assembler/Assembler.java
+++ _sp0305_2/src/main/java/assembler/Assembler.java
@@ -1,5 +1,32 @@
 package assembler;
 
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberRegisterService;
+
 public class Assembler {
+
+	private MemberDao memberDao;
+	private MemberRegisterService regSvc;
+	private ChangePasswordService pwdSvc;
+
+	public Assembler() {
+		memberDao = new MemberDao();
+		regSvc = new MemberRegisterService(memberDao);
+		pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao);
+	}
+
+	public MemberDao getMemberDao() {
+		return memberDao;
+	}
+
+	public MemberRegisterService getMemberRegisterService() {
+		return regSvc;
+	}
+
+	public ChangePasswordService getChangePasswordService() {
+		return pwdSvc;
+	}
 
 }
--- _sp0305_2/src/main/java/spring/Member.java
+++ _sp0305_2/src/main/java/spring/Member.java
@@ -1,5 +1,38 @@
 package spring;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Member {
+
+	private Long id;
+	private String email;
+	private String password;
+	private String name;
+	private LocalDateTime registerDateTime;
+
+	public Member(String email, String password, 
+			String name, LocalDateTime regDateTime) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.registerDateTime = regDateTime;
+	}
+
+
+
+	public void changePassword(String oldPassword, String newPassword) {
+		if (!password.equals(oldPassword))
+			throw new WrongIdPasswordException();
+		this.password = newPassword;
+	}
 
 }
