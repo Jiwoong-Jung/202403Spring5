fe150ab479a824ea47d0b5278cb38babad38caec
Jiwoong-Jung <jjw981119@naver.com>
2024-03-07 09:37:13 +0900

    스프링 DI 실습

--- _sp0305/src/main/java/com/board/BoardController.java
+++ _sp0305/src/main/java/com/board/BoardController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/")
+//@WebServlet("/")
 public class BoardController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
---
+++ _sp0305_2/src/main/java/spring/MemberPrinter.java
@@ -0,0 +1,10 @@
+package spring;
+
+public class MemberPrinter {
+	public void print(Member member) {
+		System.out.printf(
+				"회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n", 
+				member.getId(), member.getEmail(),
+				member.getName(), member.getRegisterDateTime());
+	}
+}
