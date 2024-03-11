55066c09e696d5606b5d688c1976928494f2a089
Jiwoong-Jung <jjw981119@naver.com>
2024-03-11 09:53:18 +0900

    예제 만들기

--- _sp0311/src/main/java/practice/MemberRegisterService.java
+++ _sp0311/src/main/java/practice/MemberRegisterService.java
@@ -1,10 +1,16 @@
 package practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MemberRegisterService {
+	@Autowired
 	private MemberDao memberDao;
 	
 	public void regist() {
 		memberDao.process();
+//		if (memberDao == null) {
+//			System.out.println("memberDao가 null입니다.");
+//		}
 		System.out.println("regist()");
 	}
 }
