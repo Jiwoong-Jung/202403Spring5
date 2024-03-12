dbd1de682b4d18160420a0f50a3abcb54710fd7c
Jiwoong-Jung <jjw981119@naver.com>
2024-03-12 16:29:02 +0900

    ajax 연습

--- _sp0312/src/main/java/servlet/GetTodos.java
+++ _sp0312/src/main/java/servlet/GetTodos.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 /**
  * Servlet implementation class GetTodos
@@ -26,8 +32,16 @@ public class GetTodos extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 할 일 목록을 가져오는 코드
+		// 이 예시에서는 단순히 두 개의 할 일을 반환합니다.
+		List<String> todos = Arrays.asList("First task", "Second task");
+
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+
+		PrintWriter out = response.getWriter();
+		out.print(new Gson().toJson(todos)); // Gson 라이브러리를 사용하여 JSON으로 변환
+		out.flush();
 	}
 
 	/**
