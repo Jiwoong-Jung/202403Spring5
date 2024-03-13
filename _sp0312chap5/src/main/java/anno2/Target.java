bb31960046aed0838c157024dea5ff3a851d937c
Jiwoong-Jung <jjw981119@naver.com>
2024-03-13 10:30:14 +0900

    스프링 5장

--- _sp0312chap5/src/main/java/anno2/Target.java
+++ _sp0312chap5/src/main/java/anno2/Target.java
@@ -5,7 +5,7 @@ import anno1.MyAnnotation;
 public class Target {
 	
     @MyAnno(value = "연습3")
-    @MyAnnotation
+    @MyAnnotation (value123 = "테스트123")
     public void sub123() {
         System.out.println("Target");
     }
