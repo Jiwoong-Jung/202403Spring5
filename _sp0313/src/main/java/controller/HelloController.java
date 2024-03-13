33c61da3bbf6ed29ca76ff60eb274df633107a41
Jiwoong-Jung <jjw981119@naver.com>
2024-03-13 12:01:45 +0900

    스프링 MVC

--- _sp0313/src/main/java/controller/HelloController.java
+++ _sp0313/src/main/java/controller/HelloController.java
@@ -1,13 +1,15 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 	
 	@GetMapping("/")
-	public String hello() {
+	public String hello(Model model) {
+		model.addAttribute("str", "한글 나오나");
 		return "hello";
 	}
 }
