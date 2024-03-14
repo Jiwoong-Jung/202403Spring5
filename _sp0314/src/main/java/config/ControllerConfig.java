1b94898cbb8209de85a616a3f94ba95022e8da86
Jiwoong-Jung <jjw981119@naver.com>
2024-03-14 15:30:49 +0900

    Spring MVC

--- _sp0314/src/main/java/config/ControllerConfig.java
+++ _sp0314/src/main/java/config/ControllerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import constroller.MyController;
+import dao.MemberDao;
+import dto.MemberDto;
 
 
 @Configuration
@@ -12,6 +14,11 @@ public class ControllerConfig {
 	@Bean
 	public MyController myController() {
 		return new MyController();
+	}
+	
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
 	}
 
 }
