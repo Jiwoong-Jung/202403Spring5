3c76bf137682ab39256a6c83a63da09a0111b048
Jiwoong-Jung <jjw981119@naver.com>
2024-03-14 10:57:24 +0900

    Spring MVC

--- _sp0314/src/main/java/config/ControllerConfig.java
+++ _sp0314/src/main/java/config/ControllerConfig.java
@@ -3,10 +3,15 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import constroller.MyController;
+
 
 @Configuration
 public class ControllerConfig {
 	
-
+	@Bean
+	public MyController myController() {
+		return new MyController();
+	}
 
 }
