f4bdde6d753cb266f2e617d524b37a2cab164059
Jiwoong-Jung <jjw981119@naver.com>
2024-03-18 10:15:22 +0900

    JdbcTemplate

---
+++ _sp0318/src/main/java/practice/Main.java
@@ -0,0 +1,15 @@
+package practice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static Logger LOGGER 
+	              = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {
+		LOGGER.info("나온다");
+
+	}
+
+}
--- _sp0318/src/main/java/practice/MemberDao.java
+++ _sp0318/src/main/java/practice/MemberDao.java
@@ -10,5 +10,12 @@ public class MemberDao {
     public MemberDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
+    
+    public int count() {
+        Integer count = jdbcTemplate
+        		.queryForObject("select count(*) from member",
+        				                        Integer.class);
+        return count;
+     }
 
 }
