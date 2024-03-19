915ab48e0eb6fb74922e89424bf07858c98db731
Jiwoong-Jung <jjw981119@naver.com>
2024-03-19 11:11:42 +0900

    chap08

---
+++ _sp0319chap8/src/main/java/main/MainForDao.java
@@ -0,0 +1,35 @@
+package main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import config.AppCtx;
+import dbquery.DbQuery;
+import spring.ChangePasswordService;
+import spring.Member;
+import spring.MemberDao;
+import spring.MemberNotFoundException;
+import spring.WrongIdPasswordException;
+
+public class MainForDao {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(MainForDao.class);
+	
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(AppCtx.class);
+
+		MemberDao dao = ctx.getBean(MemberDao.class);
+		Member member = dao.selectByEmail("madvirus@madvirus.net");
+		LOGGER.info("아이디:{}, 이메일:{}, 패스워드:{}, 이름:{}, 등록일:{}",
+				                      member.getId(), 
+				                      member.getEmail(),
+				                      member.getPassword(),
+				                      member.getName(),
+				                      member.getRegisterDateTime());
+
+		ctx.close();
+
+	}
+}
--- _sp0319chap8/src/main/java/spring/MemberDao.java
+++ _sp0319chap8/src/main/java/spring/MemberDao.java
@@ -1,10 +1,14 @@
 package spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,7 +22,22 @@ public class MemberDao {
 
 
 	public Member selectByEmail(String email) {
-		return null;
+		List<Member> results = jdbcTemplate.query(
+				"select * from MEMBER where EMAIL = ?",
+				new RowMapper<Member>() {
+					@Override
+					public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Member member = new Member(
+								rs.getString("EMAIL"),
+								rs.getString("PASSWORD"),
+								rs.getString("NAME"),
+								rs.getTimestamp("REGDATE").toLocalDateTime());
+						member.setId(rs.getLong("ID"));
+						return member;
+					}
+				}, email);
+
+		return results.isEmpty() ? null : results.get(0);
 	}
 
 	public void insert(Member member) {
